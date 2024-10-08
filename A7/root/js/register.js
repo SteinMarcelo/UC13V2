c5888fffa6e4535b03273216b4e9ec2343bc96fa
Marcelo Boeira <136658914+SteinMarcelo@users.noreply.github.com>
2024-10-07 22:43:22 -0300

    aulas-js-api

--- A7/root/js/register.js
+++ A7/root/js/register.js
@@ -5,18 +5,11 @@ document
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const senha = password;
-    const teste = 10;
-
-    bcrypt.hash(senha, teste, (err, hash) => {
-      if (err) throw err;
-      console.log(hash);
-    });
 
     const response = await fetch("http://localhost:3000/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, senha }),
+      body: JSON.stringify({ email, password }),
     });
 
     const messageElement = document.getElementById("message");
--- A7/root/server.js
+++ A7/root/server.js
@@ -1,41 +1,49 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(bodyParser.json());
 
 app.use(cors());
 
 // Conexão com o banco de dados
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'users',
-    port: '3307'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "users",
+  port: "3307",
 });
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, results) => {
-        if (err) throw err;
-        if (results.length > 0) {
-            res.sendStatus(200); // Login bem-sucedido
-        } else {
-            res.status(401).send('Credenciais inválidas');
-        }
-    });
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  db.query(
+    "SELECT * FROM users WHERE email = ? AND password = ?",
+    [email, password],
+    (err, results) => {
+      if (err) throw err;
+      if (results.length > 0) {
+        res.sendStatus(200); // Login bem-sucedido
+      } else {
+        res.status(401).send("Credenciais inválidas");
+      }
+    }
+  );
 });
 
-app.post('/register', (req, res) => {
-    const { email, password } = req.body;
-    db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, password], (err, result) => {
-        if (err) throw err;
-        res.sendStatus(201); 
-    });
+app.post("/register", (req, res) => {
+  const { email,password } = req.body;
+  db.query(
+    "INSERT INTO users (email, password) VALUES (?, ?)",
+    [email, password],
+    (err, result) => {
+      if (err) throw err;
+      res.sendStatus(201);
+    }
+  );
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+  console.log("Servidor rodando na porta 3000");
 });
